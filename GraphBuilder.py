d98a95e344c67d6bb0f9bb94ba4297c95b227ae2
vishal-pandey <contact@vishalpandey.co.in>
2024-08-19 02:07:34 +0530

    version 2

--- GraphBuilder.py
+++ GraphBuilder.py
@@ -1,5 +1,5 @@
 from langgraph.graph import StateGraph, START, END
-from MetadataToolkitNodes import list_sql_database_tool, info_sql_database_tool, generate_sql_query_tool, query_sql_database_tool, query_sql_checker_tool, generate_answer_for_user
+from MetadataToolkitNodes import list_sql_database_tool, info_sql_database_tool, generate_sql_query_tool, query_sql_database_tool, query_sql_checker_tool, generate_answer_for_user, decide_query_or_not
 from State import State
 
 GraphBuilder = StateGraph(State)
@@ -9,6 +9,12 @@ def route_tool(state: State):
         return "query_sql_checker_tool"
     else:
         return "generate_answer_for_user"
+    
+def route_query(state: State):
+    if state["is_query"] == "yes":
+        return "list_sql_database_tool"
+    else:
+        return "__end__"
 
 GraphBuilder.add_node("list_sql_database_tool", list_sql_database_tool)
 GraphBuilder.add_node("info_sql_database_tool", info_sql_database_tool)
@@ -16,9 +22,17 @@ GraphBuilder.add_node("generate_sql_query_tool", generate_sql_query_tool)
 GraphBuilder.add_node("query_sql_database_tool", query_sql_database_tool)
 GraphBuilder.add_node("generate_answer_for_user", generate_answer_for_user)
 GraphBuilder.add_node("query_sql_checker_tool", query_sql_checker_tool)
+GraphBuilder.add_node("decide_query_or_not", decide_query_or_not)
 
 
-GraphBuilder.add_edge(START, "list_sql_database_tool")
+
+GraphBuilder.add_edge(START, "decide_query_or_not")
+
+GraphBuilder.add_conditional_edges(
+    "decide_query_or_not",
+    route_query
+)
+
 GraphBuilder.add_edge("list_sql_database_tool", "info_sql_database_tool")
 GraphBuilder.add_edge("info_sql_database_tool", "generate_sql_query_tool")
 GraphBuilder.add_edge("generate_sql_query_tool", "query_sql_database_tool")
--- MetadataToolkitNodes.py
+++ MetadataToolkitNodes.py
@@ -216,3 +216,26 @@ def generate_answer_for_user(state: State):
     state["messages"].append(AIMessage(content=state["final_answer"]))
     
     return state
+
+
+
+# function to take user input and decide if it can be answered by sql query or not?
+def decide_query_or_not(state: State):
+    """Decide if the user query can be answered by a SQL query.
+        Check the user query and decide if it can be answered by a SQL query."""
+    
+    state["messages"].append(HumanMessage(content=state["question"]))
+    state["messages"].append(SystemMessage(content="You are an expert data steward working on openmetadata system looking at the question asked by the user check if it can be answered by the query or not. State the answer in yes then reutrn only 'yes' as string, else answer the question of the user based on the history of the conversation."))
+    
+    prompt = ChatPromptTemplate.from_messages(state["messages"])
+    chain = prompt | model
+    output = chain.invoke({})
+    response = output.content
+    if response == "yes":
+        state["is_query"] = "yes"
+    else:
+        state["is_query"] = "no"
+        state["messages"].pop()
+        state["messages"].append(AIMessage(content=response))
+
+    return state  
